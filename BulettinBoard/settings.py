77475237960e8705fef565ecbd5cecb62d2e6d47
Tomasz Izdebski <lukas4096@gmail.com>
2022-10-02 22:21:28 +0200

    testowe wdrożenie projektu na heroku

--- BulettinBoard/settings.py
+++ BulettinBoard/settings.py
@@ -16,7 +16,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ SECRET_KEY = 'rtm$@rcneyyjbo$u+)@z1dxy!qrtxc676h&zh97s+46)=2)@9*'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -93,7 +91,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
@@ -105,7 +102,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-#USE_TZ = True
+# USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
@@ -126,3 +123,9 @@ except ModuleNotFoundError:
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+#
+LOGIN_URL = 'users:login'
+import django_heroku
+django_heroku.settings(locals())
+
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn BulettinBoard.wsgi --log-file -
